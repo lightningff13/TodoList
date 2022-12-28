c170be000ddfffb0a2290658e2756c0afb4b0c64
charly.brugere <charly.brugere@viseo.com>
2022-12-28 17:04:48 +0100

    migrate to Jetpack Compose

--- app/src/main/java/com/personal/todolist/ui/MainActivity.kt
+++ app/src/main/java/com/personal/todolist/ui/MainActivity.kt
@@ -1,19 +1,48 @@
 package com.personal.todolist.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
-import com.personal.todolist.R
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.personal.todolist.ui.ui.theme.TodoListTheme
 import com.personal.todolist.ui.viewModels.GetTodoListsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-
-    private val viewModel: GetTodoListsViewModel by viewModels()
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            TodoListTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    TodoLists()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TodoLists(viewModel: GetTodoListsViewModel = hiltViewModel()) {
+    //val todoLists by viewModel.todoListsFlow.collectAsState()
+    Text(text = "Hello World")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TodoListsPreview() {
+    TodoListTheme {
+        TodoLists()
     }
 }
